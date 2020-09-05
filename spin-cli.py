0b6e3a67c2771b28e7351b9ff2d51d481b15b9e4
Karthigayan Devan <karthigayan.devan@macys.com>
2020-09-05 16:01:29 -0400

    clean up script

--- spin-cli.py
+++ spin-cli.py
@@ -66,53 +66,41 @@ def getAppPipelineJsonUsingConfigFile():
     for pipe in appPipelines:
         cmd = f"spin pipeline get --name {pipe['pipeline']} --application {pipe['application']}"
         pipelineJson = json.loads(getProcessOutput(cmd))
-
         if pipelineJson is not None:
             if not os.path.exists(f"spin-existing-pipelines/{pipe['application']}"):
                 os.makedirs(f"spin-existing-pipelines/{pipe['application']}")
             with open(f"spin-existing-pipelines/{pipe['application']}/{pipe['pipeline']}.json", 'w') as outfile:
                 json.dump(pipelineJson, outfile)
 
-
 def updateAppPipeline():
     pipeDirectory='spin-existing-pipelines/'
-
     if os.path.isdir(pipeDirectory):
         appPipelineNames = getAppPipelineNames()
         appNames=[]
-
         for app in appPipelineNames:
             appNames.append(app['application'])
         filterAppNames =  list(set(appNames))
-
         for app in filterAppNames:
             fileList = glob.glob(f"spin-existing-pipelines/{app}/*.json")
             for file in fileList:
                 print (f'application:{app} - pipeline:{file}',  getProcessOutput(f"spin pipeline save --file {file}"))
     else:
         print ('Pipeline files are missing. First get files and try again. Type `help` to know about details')
-
 
 def updateAppPipelineUsingConfigFile():
     pipeDirectory='spin-existing-pipelines/'
-
     if os.path.isdir(pipeDirectory):
         appPipelineNames = getAppPipelineNamesUsingConfigFile()
         appNames=[]
-
         for app in appPipelineNames:
             appNames.append(app['application'])
         filterAppNames =  list(set(appNames))
-
-        print (filterAppNames)
-
         for app in filterAppNames:
             fileList = glob.glob(f"spin-existing-pipelines/{app}/*.json")
             for file in fileList:
                 print (f'application:{app} - pipeline:{file}',  getProcessOutput(f"spin pipeline save --file {file}"))
     else:
         print ('Pipeline files are missing. First get files and try again. Type `help` to know about details')
-
 
 arg = sys.argv[1]
 
